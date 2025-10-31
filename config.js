cd7e0d8951cec0f2c446ae966b9f804c80e28eb8
thuocthucphamsuckhoe-boop <thuocthucpham.suckhoe@gmail.com>
2025-10-31 17:38:16 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const LINKS = {
+  lichUBND: "https://docs.google.com/spreadsheets/d/xxxx",
+  lichPhong: "https://docs.google.com/spreadsheets/d/yyyy",
+  trongTam: "https://docs.google.com/spreadsheets/d/zzzz",
+  cbcc: "https://docs.google.com/spreadsheets/d/aaaa",
+  baoCao: "https://docs.google.com/document/d/bbbb"
+};
